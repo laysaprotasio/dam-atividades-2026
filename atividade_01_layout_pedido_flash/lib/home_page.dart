9d67874406d78655b3475e18998576b3a17329b5
Laysa Protasio <laysaprotasio@gmail.com>
2026-07-06 23:26:55 -0300

    feat(DAM-A1) - Adição de Menu utilizando drawer

--- atividade_01_layout_pedido_flash/lib/home_page.dart
+++ atividade_01_layout_pedido_flash/lib/home_page.dart
@@ -6,12 +6,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pedido Flash'),
+      appBar: AppBar(title: const Text('Pedido Flash')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            SizedBox(
+              height: 160,
+              child: Center(child: Text('Menu Caixa')),
+            ),
+            ListTile(
+              leading: Icon(Icons.add_shopping_cart),
+              title: Text('Novo Pedido'),
+            ),
+            ListTile(leading: Icon(Icons.history), title: Text('Histórico')),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Configurações'),
+            ),
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text('Cardápio'),
-      ),
+      body: const Center(child: Text('Cardápio')),
     );
   }
 }
