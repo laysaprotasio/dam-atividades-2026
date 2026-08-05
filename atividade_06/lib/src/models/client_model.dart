11ed8ff1ef47f86c492f64b99b9b77a32b1b3020
Laysa Protasio <laysaprotasio@gmail.com>
2026-08-04 22:56:57 -0300

    feat(DAM-06) - Adição de birthDate ao ClientModel

--- atividade_06/lib/src/models/client_model.dart
+++ atividade_06/lib/src/models/client_model.dart
@@ -5,12 +5,14 @@ class ClientModel {
   final String name;
   final String email;
   final String phone;
+  final DateTime? birthDate;
 
   ClientModel({
     String? id,
     required this.name,
     required this.email,
     required this.phone,
+    this.birthDate,
   }) : id = id ?? const Uuid().v4();
 
   ClientModel copyWith({
@@ -18,12 +20,14 @@ class ClientModel {
     String? name,
     String? email,
     String? phone,
+    DateTime? birthDate,
   }) {
     return ClientModel(
       id: id ?? this.id,
       name: name ?? this.name,
       email: email ?? this.email,
       phone: phone ?? this.phone,
+      birthDate: birthDate ?? this.birthDate,
     );
   }
 }
